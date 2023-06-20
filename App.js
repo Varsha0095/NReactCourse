2da01e65e68d40a13ea54bae7e00ffe83746de95
Varsha0095 <Varsha0095@users.noreply.github.com>
2023-06-20 18:15:47 +0530

    container using createElement

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement(
     "h1",
     {
-      id: "title",
+      id: "title1",
+      key: "title1",
     },
-    "Hello World By React!"
+    "This is heading 1"
   );
 
   const heading2 = React.createElement('h2', {
-    id: 'title2'
-  }, 'Heya!!')
+    id: 'title2',
+    key: 'title2'
+  }, 'This is heading 2')
+
+  const heading3 = React.createElement('h3', {
+    id: 'title3',
+    key: 'title3'
+  },
+  'This is heading 3')
 
   const container = React.createElement('div',{
-    id: 'container'
-  }, [heading, heading2])
+    id: 'container',
+    className: 'title',
+  }, [heading, heading2, heading3]);
   console.log(heading);
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(container);
